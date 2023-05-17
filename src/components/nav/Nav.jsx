3154407dc2f19fb88add639bd0802f44ccfde444
Aakash Thapa Chhetri (Skyknew) <77266633+Skyknew@users.noreply.github.com>
2023-05-17 12:00:30 +0545

    ok

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageDetail} from 'react-icons/bi' 
+import {useState } from 'react'
+
+const Nav = () => {
+  const [activeNav,setActiveNav] = useState('#')
+  return (
+    <nav>
+        <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}> <AiOutlineHome/> </a>
+
+        <a href="#about" onClick={() => setActiveNav('#')} className={activeNav === '#about' ?'active' : ''}> <AiOutlineUser/> </a>
+
+        <a href="#experience" onClick={() => setActiveNav('#')} className={activeNav === '#experience' ?'active' : ''}> <BiBook/> </a>
+
+        <a href="#services" onClick={() => setActiveNav('#')} className={activeNav === '#services' ?'active' : ''}> <RiServiceLine/> </a>
+
+        <a href="#contact" onClick={() => setActiveNav('#')} className={activeNav === '#contact' ?'active' : ''}> <BiMessageDetail/> </a>     
+
+    </nav> 
+  
+  )
+}
+
+export default Nav
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+import './services.css'
+import {BiCheck} from 'react-icons/bi'
+
+const Services = () => {
+  return (
+    <section id='services'> 
+      <h5> What I Offer </h5>    
+      <h2> Services </h2>
+
+      <div className="container services__container">
+        <article className="Services">
+          <div className="service__head">
+            <h3> UI/UX Design </h3>          
+          </div> 
+
+          <u1 className="service__list">
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+          </u1>
+        </article>
+        {/* END Of UI/UX  */}
+
+        <article className="Services">
+          <div className="service__head">
+            <h3> Web Development </h3>          
+          </div> 
+
+          <u1 className="service__list">
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+            
+          </u1>
+        </article>
+
+        {/* END OF Web Development  */}
+
+
+        <article className="Services">
+          <div className="service__head">
+            <h3> Content Creation </h3>          
+          </div> 
+
+          <u1 className="service__list">
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+
+            <li> 
+              <BiCheck className='service__list-icon' />
+              <p> nslnln akskan a kanskan </p>
+            </li>
+            
+          </u1>
+        </article>
+
+        {/* END OF Content Creation  */}
+
+
+      </div>
+    
+    
+    </section>
+  )
+}
+
+export default Services
